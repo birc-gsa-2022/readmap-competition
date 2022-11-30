24778e18dcbd172c428fafaae2976727879bfd39
Thomas Mailund <mailund@birc.au.dk>
2022-11-30 12:01:46 +0100

    run Wed Nov 30 12:01:46 CET 2022

--- config.sh
+++ config.sh
@@ -23,13 +23,13 @@ mydirs=(
 gsa_rep=20  # Number of repeats when measuring performance
 
 # Config. for full run of all teams
-gsa_genome_lens=(1000 2000)
+gsa_genome_lens=(2500 5000)
 gsa_reads_lens=(50)
 
 # Config for the faster ones
 long_teams=(
     project-5-python-armando-christian-perez
-    #project-5-c-o-no
+    project-5-c-o-no
 )
-gsa_long_genome_lens=(10000 20000)
+gsa_long_genome_lens=(25000 50000)
 gsa_long_reads_lens=(200)
